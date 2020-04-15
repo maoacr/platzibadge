8fecc6b17d283752ab89eff4898e1eec96c20baa
maoacr <maoacr@gmail.com>
2020-04-14 22:54:12 -0500

    Updates

---
+++ precache-manifest.ffdfe41615607d264d75e63ae10596a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04718385f287878fc61ee28c310260c3",
+    "url": "/platzibadge/index.html"
+  },
+  {
+    "revision": "9e863ff8292274f0a70e",
+    "url": "/platzibadge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4662b3ff51c6886cce15",
+    "url": "/platzibadge/static/js/2.d134ae20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/platzibadge/static/js/2.d134ae20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e863ff8292274f0a70e",
+    "url": "/platzibadge/static/js/main.8f468d6e.chunk.js"
+  },
+  {
+    "revision": "c53e6b657d287cd2bc47",
+    "url": "/platzibadge/static/js/runtime-main.251346a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/platzibadge/static/media/logo.5d5d9eef.svg"
+  }
+]);
